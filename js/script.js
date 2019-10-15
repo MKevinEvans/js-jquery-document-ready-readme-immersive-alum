b0629d8e0c469e5052a1a3a1702f861906b5f066
Kevin Evans <mkevinevans@gmail.com>
2019-10-15 15:48:09 +0000

    Automatically backed up by Learn

--- js/script.js
+++ js/script.js
@@ -2,3 +2,7 @@ document.addEventListener('DOMContentLoaded', function(){
       console.log("loaded");
      $("#text").append("this is so freaking cool.");
 })()
+
+$(document).ready(function(){
+     $("#text").append("this is so freaking cool.");
+})
